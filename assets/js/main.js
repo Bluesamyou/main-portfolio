908993236f015b62c78f13a7b2eb6033770551d5
Shervantha Nirmal Hugh De Silva <shevids@gmail.com>
2019-07-22 00:51:48 +1000

    Minor Style updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 const card = document.querySelector('.card');
+const avatar = document.querySelector('#avatar');
 
 const t1 = new TimelineMax();
 
-t1.fromTo(card,1, {opacity : "0", height: "0vh"}, {opacity : "1", height: "100vh", ease: Power2.easeInOut});
+t1.fromTo(card,2, {opacity : "0", height: "0vh"}, {opacity : "1", height: "100vh", ease: Power2.easeInOut})
+    .fromTo(avatar, 2, {borderRadius : "0px"}, {borderRadius : "25px" , ease: Power2.easeInOut}, "-=2")
