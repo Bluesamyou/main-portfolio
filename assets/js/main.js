68fbfd4ca7fa1d840af7f50c0bcb46c61a6a61a9
Shervantha Nirmal Hugh De Silva <shevids@gmail.com>
2019-07-22 00:35:12 +1000

    updated mobile css

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,4 @@ const card = document.querySelector('.card');
 
 const t1 = new TimelineMax();
 
-t1.fromTo(card,1, {opacity : "0", height: "0%"}, {opacity : "1", height: "100vh", ease: Power2.easeInOut});
+t1.fromTo(card,1, {opacity : "0", height: "0vh"}, {opacity : "1", height: "100vh", ease: Power2.easeInOut});
