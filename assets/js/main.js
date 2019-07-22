7f352d8092b506da565c66a7fcf2d7af6bfadf99
Shervantha Nirmal Hugh De Silva <shevids@gmail.com>
2019-07-22 11:30:38 +1000

    slight layout improvements

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,17 @@
 var typed = new Typed(".typed", {
-    strings: ["Fullstack Developer", "Frontend Expert","Web Automation Tester"],
+    strings: ["Fullstack Developer", "Frontend Expert","Node Server and API developer","Web Automation Tester"],
     backSpeed : 30, 
     typeSpeed : 40, 
-    loop : true
+    loop : true,
+    cursorChar: '|',
 
   });
 
-const card = document.querySelector('.card');
+const card = document.querySelector('.jumbotron');
 const avatar = document.querySelector('#avatar');
 
 const t1 = new TimelineMax();
 
-t1.fromTo(card,2, {opacity : "0", height: "0vh"}, {opacity : "1", height: "100vh", ease: Power2.easeInOut})
+t1.fromTo(card,2, {opacity : "0", height: "0%"}, {opacity : "1", height: "100%", ease: Power2.easeInOut})
     .fromTo(avatar, 2, {borderRadius : "0px"}, {borderRadius : "25px" , ease: Power2.easeInOut}, "-=2")
 
