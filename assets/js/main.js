344ccddd044697b0419f3f5cd8eb5622dc7d9d83
Shervantha Nirmal Hugh De Silva <shevids@gmail.com>
2019-07-22 11:13:02 +1000

    Added typed.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,11 @@
+var typed = new Typed(".typed", {
+    strings: ["Fullstack Developer", "Frontend Expert","Web Automation Tester"],
+    backSpeed : 30, 
+    typeSpeed : 40, 
+    loop : true
+
+  });
+
 const card = document.querySelector('.card');
 const avatar = document.querySelector('#avatar');
 
@@ -5,3 +13,4 @@ const t1 = new TimelineMax();
 
 t1.fromTo(card,2, {opacity : "0", height: "0vh"}, {opacity : "1", height: "100vh", ease: Power2.easeInOut})
     .fromTo(avatar, 2, {borderRadius : "0px"}, {borderRadius : "25px" , ease: Power2.easeInOut}, "-=2")
+
