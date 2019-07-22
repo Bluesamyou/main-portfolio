653b8a6a5dfbd15197cbf683773f008c6c8ce854
Shervantha Nirmal Hugh De Silva <shevids@gmail.com>
2019-07-22 13:55:16 +1000

    Added tools used

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,11 +7,11 @@ var typed = new Typed(".typed", {
 
   });
 
-const card = document.querySelector('.jumbotron');
-const avatar = document.querySelector('#avatar');
+// const card = document.querySelector('.jumbotron');
+// const avatar = document.querySelector('#avatar');
 
-const t1 = new TimelineMax();
+// const t1 = new TimelineMax();
 
-t1.fromTo(card,2, {opacity : "0", height: "0%"}, {opacity : "1", height: "100%", ease: Power2.easeInOut})
-    .fromTo(avatar, 2, {borderRadius : "0px"}, {borderRadius : "25px" , ease: Power2.easeInOut}, "-=2")
+// t1.fromTo(card,2, {opacity : "0", height: "0%"}, {opacity : "1", height: "100%", ease: Power2.easeInOut})
+//     .fromTo(avatar, 2, {borderRadius : "0px"}, {borderRadius : "25px" , ease: Power2.easeInOut}, "-=2")
 
