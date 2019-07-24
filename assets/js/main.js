00633a04624b171db0d93063b2bdd5e5dbd07cd2
Shevi <shevids@gmail.com>
2019-07-24 21:01:14 +1000

    Added more subtle animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,10 +23,31 @@ var scene = new Scene({
             transform : "scale(1)", 
             opacity : 1
         }
+    }, 
+    ".cta" : function(index){
+        return {
+            0: {
+                opacity : 0,
+                transform : "translate(0px, 0px)"
+                
+            },
+            0.5 : {
+                opacity : 0.5,
+                transform : "translate(0px, -20px)"
+            },
+            1:{
+                opacity : 1,
+                transform : "translate(0px, 0px)"
+            }, 
+            options : {
+                delay : index * 0.5, 
+
+            }
+        }
     }
 }, 
 {
-    duration : 0.7,
+    duration : 2,
     easing : Scene.EASE_IN_OUT,
     selector: true, 
 }).playCSS();
