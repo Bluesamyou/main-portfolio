beb66232c98c58ccb6374e523b4dd0d264e97a08
Shevi <shevids@gmail.com>
2019-08-12 23:09:35 +1000

    Added in portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 var rellax = new Rellax('.rellax');
 
+AOS.init();
+
 var scene = new Scene({
     '.personal-card' : {
         0: {
@@ -57,58 +59,6 @@ var scene = new Scene({
     selector: true, 
 }).playCSS();
 
-randNumber = () => {
-    console.log(`${(Math.floor(Math.random())*70)+10}px`)
-    return `${(Math.floor(Math.random)*70)+10}px`
-}
-
-var sceneInf = new Scene({
-    '.dev-icon' : function(i){
-        return {
-            0 : {
-                position : 'absolute',
-                left : randNumber(),
-                opacity : "0",
-                transform : "translate(0px,0px)"
-            }, 1:{
-                position : 'absolute',
-                left : randNumber(),
-                opacity : "1",
-                transform : `translate(0px,-${(Math.floor(Math.random()*10000))}px)`
-            }, 
-            options : {
-                delay  : i * 1
-            }
-        }
-    }
-}, 
-{
-    duration : 10, 
-    easing : Scene.EASE_IN_OUT,
-    iterationCount: "infinite",
-    selector: true, 
-}).playCSS()
-
-
-var testAnim = new Scene({
-    '#code-1' : {
-        0:{
-            width: "0px"
-        },
-        1:{
-            width : "39px"
-        }
-    },
-
-},{
-    selector: true, 
-    easing : Scene.EASE_IN_OUT, 
-    iterationCount: "infinite", 
-    duration : 3
-    
-}).playCSS()
-
-
 var typed = new Typed(".typed", {
     strings: ["Fullstack Developer", "Frontend Expert","Backend Guru","Web Automation Tester"],
     backSpeed : 40, 
@@ -117,3 +67,32 @@ var typed = new Typed(".typed", {
     cursorChar: '|',
   });
 
+
+  var infiniteSpinner = new Scene({
+      '.devicon' : function(i){
+          return ({
+            0 : {
+              transform : "scale(1)"
+  
+            },
+            0.5 : {
+              transform : "scale(1.2)"
+              
+            }, 
+            1: {
+              transform : "scale(1)"
+              
+            }, 
+            options : {
+                delay : i*1
+            }
+        })
+
+      }
+
+      
+  }, {
+    duration : 2, 
+    iterationCount: "infinite",
+    selector: true, 
+  }).playCSS()
