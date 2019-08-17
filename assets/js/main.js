51eb0b0bf8ad4a515641940b8d08ec52f6aaaf35
Shevi <shevids@gmail.com>
2019-08-17 20:03:31 +1000

    Added CTA

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,98 +1,106 @@
-var rellax = new Rellax('.rellax');
+$(document).ready(() => {
+    var rellax = new Rellax('.rellax');
 
-AOS.init();
+    AOS.init();
 
-var scene = new Scene({
-    '.personal-card' : {
-        0: {
-            height : "0px",
-            transform : "scale(0.7)", 
-            opacity : 0
-        },
-
-        .5 : {
-            height : "100%",
-            transform : "scale(1.2)", 
-        },
-
-        1:{
-            height : "100%",
-            transform : "scale(1)", 
-            opacity : 1
-        }
-    }, 
-    '.avatar ' : {
-        0: {
-            transform : "scale(0)", 
-            opacity : 0
-        },
-        1:{
-            transform : "scale(1)", 
-            opacity : 1
-        }
-    }, 
-    ".cta" : function(index){
-        return {
+    var scene = new Scene({
+        '.personal-card': {
             0: {
-                opacity : 0,
-                transform : "translate(0px, 0px)"
-                
+                height: "0px",
+                transform: "scale(0.7)",
+                opacity: 0
             },
-            0.5 : {
-                opacity : 0.5,
-                transform : "translate(0px, -20px)"
-            },
-            1:{
-                opacity : 1,
-                transform : "translate(0px, 0px)"
-            }, 
-            options : {
-                delay : index * 0.5
 
+            .5: {
+                height: "100%",
+                transform: "scale(1.2)",
+            },
+
+            1: {
+                height: "100%",
+                transform: "scale(1)",
+                opacity: 1
+            }
+        },
+        '.avatar ': {
+            0: {
+                transform: "scale(0)",
+                opacity: 0
+            },
+            1: {
+                transform: "scale(1)",
+                opacity: 1
+            }
+        },
+        ".cta": function (index) {
+            return {
+                0: {
+                    opacity: 0,
+                    transform: "translate(0px, 0px)"
+
+                },
+                0.5: {
+                    opacity: 0.5,
+                    transform: "translate(0px, -20px)"
+                },
+                1: {
+                    opacity: 1,
+                    transform: "translate(0px, 0px)"
+                },
+                options: {
+                    delay: index * 0.5
+
+                }
             }
         }
-    }
-}, 
-{
-    duration : 2,
-    easing : Scene.EASE_IN_OUT,
-    selector: true, 
-}).playCSS();
+    }, {
+        duration: 2,
+        easing: Scene.EASE_IN_OUT,
+        selector: true,
+    }).playCSS();
 
-var typed = new Typed(".typed", {
-    strings: ["Fullstack Developer", "Frontend Expert","Backend Guru","Web Automation Tester"],
-    backSpeed : 40, 
-    typeSpeed : 50, 
-    loop : true,
-    cursorChar: '|',
-  });
+    var typed = new Typed(".typed", {
+        strings: ["Fullstack Developer", "Frontend Expert", "Backend Guru", "Web Automation Tester"],
+        backSpeed: 40,
+        typeSpeed: 50,
+        loop: true,
+        cursorChar: '|',
+    });
 
 
-  var infiniteSpinner = new Scene({
-      '.devicon' : function(i){
-          return ({
-            0 : {
-              transform : "scale(1)"
-  
-            },
-            0.5 : {
-              transform : "scale(1.2)"
-              
-            }, 
-            1: {
-              transform : "scale(1)"
-              
-            }, 
-            options : {
-                delay : i*1
-            }
-        })
+    var infiniteSpinner = new Scene({
+        '.devicon': function (i) {
+            return ({
+                0: {
+                    transform: "scale(1)"
 
-      }
+                },
+                0.5: {
+                    transform: "scale(1.2)"
 
-      
-  }, {
-    duration : 2, 
-    iterationCount: "infinite",
-    selector: true, 
-  }).playCSS()
+                },
+                1: {
+                    transform: "scale(1)"
+
+                },
+                options: {
+                    delay: i * 1
+                }
+            })
+
+        }
+
+
+    }, {
+        duration: 2,
+        iterationCount: "infinite",
+        selector: true,
+    }).playCSS()
+
+
+    $('#google').on('click', function (evt) {
+        evt.preventDefault()
+
+        window.location.href = "mailto:mail@example.org";
+    })
+})
